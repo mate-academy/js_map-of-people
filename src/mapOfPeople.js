9b8144932f92bb0a2635b2d6c3045c9ece846166
unknown <mariia>
2019-06-19 23:57:53 +0300

    'Solution'

--- src/mapOfPeople.js
+++ src/mapOfPeople.js
@@ -40,7 +40,15 @@
  * @return {Map}
  */
 function mapOfPeople(people) {
-  // write code here
+  const mapOfPeopleInRoom = new Map();
+  for (const elem in people) {
+    if (mapOfPeopleInRoom.has(people[elem])) {
+      mapOfPeopleInRoom.delete(people[elem]);
+    } else {
+      mapOfPeopleInRoom.set(people[elem], Number(elem));
+    }
+  }
+  return mapOfPeopleInRoom;
 }
 
 module.exports = mapOfPeople;
