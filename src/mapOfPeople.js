540ea12daca681c29b1017400c1d1e08312a0ce3
Andrii <farikpite@gmail.com>
2019-10-04 12:17:24 +0300

    Solution

--- src/mapOfPeople.js
+++ src/mapOfPeople.js
@@ -3,32 +3,32 @@
 /**
  * Implement mapOfPeople function:
  *
- * You are given an array of numbers. Each of the numbers represents a i. A
+ * You are given an array of numbers. Each of the numbers represents a person. A
  * number in the array may occur just once or it may occur several times, in
- * which case it denotes the same i every time it occurs.
+ * which case it denotes the same person every time it occurs.
  *
  * You also have a room. Initially the room is empty. Then people start to come
  * into the room in the order they appear in the array. More accurately, if the
- * next number in the array is N, the i N enters the room if he wasn't
- * already in the room or leaves the room in case the i was already in the
+ * next number in the array is N, the person N enters the room if he wasn't
+ * already in the room or leaves the room in case the person was already in the
  * room at that point. A number at position S in the input array corresponds to
  * the Sth second.
  *
  * For instance, if the input array is [1, 3, 2, 1, 2, 1], it means that the
  * following has occurred:
  *
- * — i 1 entered the room at 0 seconds;
- * — i 3 entered the room at 1 second;
- * — i 2 entered the room at 2 seconds;
- * — i 1 left the room at 3 seconds;
- * — i 2 left the room at 4 seconds;
- * — i 1 reentered the room at 5 seconds.
+ * — person 1 entered the room at 0 seconds;
+ * — person 3 entered the room at 1 second;
+ * — person 2 entered the room at 2 seconds;
+ * — person 1 left the room at 3 seconds;
+ * — person 2 left the room at 4 seconds;
+ * — person 1 reentered the room at 5 seconds.
  *
  * Return a Map that contains the people currently in the room and stores the
  * time they entered. People who have entered earlier should occur earlier in
  * the Map that you return. For the example above, the correct answer is a Map
- * containing elements 3, 1 (in that order) with map[3] === 1 (as i 3
- * entered at 1 second) and map[1] === 5 (as the last time i 1 entered the
+ * containing elements 3, 1 (in that order) with map[3] === 1 (as person 3
+ * entered at 1 second) and map[1] === 5 (as the last time person 1 entered the
  * room was at 5 seconds). Return an empty map if there are no people in the
  * room.
  *
@@ -41,11 +41,11 @@
  */
 function mapOfPeople(people) {
   const map = new Map();
-  for (let i = 0; i < people.length; i++) {
-    if (map.has(people[i])) {
-      map.delete(people[i]);
+  for (let per = 0; per < people.length; per++) {
+    if (map.has(people[per])) {
+      map.delete(people[per]);
     } else {
-      map.set(people[i], i);
+      map.set(people[per], per);
     }
   }
   return map;
