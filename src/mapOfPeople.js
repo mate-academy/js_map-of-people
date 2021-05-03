8eab1b2dd8f2061dc6d0740011beb504829d0bf1
Vitalii Siedykh <siedykh.v@gmail.com>
2021-05-03 10:36:37 +0300

    solution

--- src/mapOfPeople.js
+++ src/mapOfPeople.js
@@ -40,7 +40,17 @@
  * @return {Map}
  */
 function mapOfPeople(people) {
-  // write code here
+  const final = new Map();
+
+  for (let i = 0; i < people.length; i++) {
+    if (final.has(people[i])) {
+      final.delete(people[i]);
+    } else {
+      final.set(people[i], i);
+    }
+  }
+
+  return final;
 }
 
 module.exports = mapOfPeople;
