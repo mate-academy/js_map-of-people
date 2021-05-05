dfa595c1d5c3bb895c5d345bd4867a2007d48661
Andrey Dorohov <andrey.dorohov8@gmail.com>
2021-05-05 17:11:13 +0300

    done

--- src/mapOfPeople.js
+++ src/mapOfPeople.js
@@ -40,7 +40,17 @@
  * @return {Map}
  */
 function mapOfPeople(people) {
-  // write code here
+  const attendance = new Map();
+
+  for (let i = 0; i < people.length; i++) {
+    if (attendance.has(people[i])) {
+      attendance.delete(people[i]);
+    } else {
+      attendance.set(people[i], i);
+    }
+  }
+
+  return attendance;
 }
 
 module.exports = mapOfPeople;
